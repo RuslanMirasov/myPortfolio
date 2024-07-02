bac7f5fe587d4035c90ac1b46cff8635d67f9915
Mirasov <info@mirasov.dev>
2024-07-02 03:29:07 +0200

    sdsdsd

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,4 @@
 import css from './Logo.module.scss';
-import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { NavLink } from 'react-router-dom';
 import { usePopup } from 'contexts/PopupContext';
@@ -9,28 +8,10 @@ const Logo = () => {
   const { menuClose } = usePopup();
   const { t } = useTranslation();
 
-  useEffect(() => {
-    const spinner = document.querySelector(`[data-name="spinner"]`);
-    const handleScroll = () => {
-      if (spinner) {
-        spinner.style.transform = `rotate(${window.scrollY / 8}deg)`;
-      }
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
     <NavLink to="/" className={css.Logo} onClick={menuClose}>
       <div className={css.IconWrapper}>
-        <div className={css.LogoIcon}>
-          <Icon name="logo" size={26} />
-        </div>
-        <div className={css.SpinnerIcon} data-name="spinner">
-          <Icon name="spin" />
-        </div>
+        <Icon name="logo" size={50} />
       </div>
       <div className={css.LogoText}>
         <p>Front-End</p>
